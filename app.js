5061475e58f995bc075d4cc3a046b25b1791d9c0
mubashirzebi <mubashshirzebi@gmail.com>
2022-11-10 17:45:38 +0530

    add snyk and helmet

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const helmet = require('helmet');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const {connect} = require('./src/db');
@@ -13,12 +13,15 @@ const { SystemConfig } = require('./src/sync/configs/system');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(cors());
+
+app.use(cors(
+  {
+    origin: ['https://316.synthex.finance']
+}
+));
 app.use(logger('dev')); 
 app.use(express.json());
+app.use(helmet())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
